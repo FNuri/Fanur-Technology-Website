7d5e53c3d96c4ef2412bc7a705c74d9e47bb3410
FNuri <fowsihashi@hotmail.co.uk>
2020-06-25 00:04:55 +0300

    Edited styling of the thankyou page

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
         /* Check the location of each desired element */
         $('.fade').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            let bottom_of_object = $(this).position().top + $(this).outerHeight();
+            let bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it in */
             if( bottom_of_window > bottom_of_object ){
